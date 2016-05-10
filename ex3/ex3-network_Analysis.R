ff359ef91d08e116d463dfebcaa7705a667eb48e
ohad <master.gefen@gmail.com>
2016-05-10 18:02:39 +0300

    update ex3

--- ex3/ex3-network_Analysis.R
+++
@@ -1,9 +0,0 @@
-library(igraph)
-ga.data <- read.csv('~/R/ga_edgelist.csv', header=TRUE)
-g <- graph.data.frame(ga.data, directed=FALSE)
-summary(g)
-
-V(g)$name
-
-g$layout <- layout.fruchterman.reingold(g)
-plot(g)
